d8fd52558d4af4117b95d7f3c400311b0c25d0e3
ZhiJun Gu <ghijnuuz@gmail.com>
2014-04-14 18:04:50 +0800

    提交文件整理脚本，100个文件存入一个目录

---
+++ OrganizeFiles.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# 文件整理脚本
+# 如果目录下文件数超过100，则每100个文件放入新建的子目录中
+
+import sys
+import os
+import shutil
+
+def getsubpath(filepath):
+    isfindpath = False
+    pathnum = 1
+    while isfindpath == False:
+        subpath = os.path.join(filepath, str(pathnum))
+        if os.path.exists(subpath) == False:
+            os.mkdir(subpath)
+            isfindpath = True
+            return subpath
+        pathnum = pathnum + 1
+
+def main():
+    filelist = []
+    filecount = 100
+    filepath = os.getcwd()
+    selffilename = 'OrganizeFiles.py'
+
+    # Get file list
+    v = os.walk(filepath)
+    for root, dirs, files in v:
+        filelist = files
+        break
+
+    i = 1
+    while filecount * i < len(filelist):
+        # get sub path's files
+        subfilelist = filelist[(i-1)*filecount:i*filecount]
+        subpath = getsubpath(filepath)
+        # move files
+        for f in subfilelist:
+            if f != selffilename:
+                shutil.move(os.path.join(filepath, f), os.path.join(subpath, f))
+        i = i + 1
+
+if __name__ == '__main__':
+    main()
